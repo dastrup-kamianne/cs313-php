edea02e7b1fb9df619e9230177f189640b420006
Dastrup's <Dastrup's>
2016-05-05 13:33:04 -0600

    restarting survey app

--- assignments/survey/results.php
+++ assignments/survey/results.php
@@ -8,22 +8,24 @@
       <div class="wrapper">
      <header>
          <?php include $_SERVER['DOCUMENT_ROOT'].'/modules/main-header.php'; ?>
-         <h1>Which Frozen Character Are You?</h1>
+         <h1>Results</h1>
      </header>
           <main>   
-      <p>
-          <?php if ($answer !== NULL)
-              {echo $name . ', you are most like' . $answer;} ?>
-          
-      </p><br>
-      <?php echo $anna?>% of users are like Anna.<br>
-      <?php echo $elsa?>% of users are like Elsa.<br>
-      <?php echo $bulda?>% of users are like Bulda.<br>
-      <?php echo $queen?>% of users are like Queen Iduna.<br>
-      <?php echo $kristoff?>% of users are like Kristoff.<br>
-      <?php echo $olaf?>% of users are like Olaf.<br>
-      <?php echo $hans?>% of users are like Hans.<br>
-      <?php echo $oaken?>% of users are like Oaken.<br>
+      <p>Thank you, <?php echo $name?>!
+      <?php echo count($male) ?> users are male.<br>
+      <?php echo count($female)?> users are like female.<br>
+      <?php echo count($teen) ?> users are 0-19.<br>
+      <?php echo count($twenty) ?> users are 20-29.<br>
+      <?php echo count($thirty) ?> users are 30-39.<br>
+      <?php echo count($forty) ?> users are 40-49.<br>
+      <?php echo count($fifty) ?> users are 50-59.<br>
+      <?php echo count($sixty) ?> users are 60-69.<br>
+      <?php echo count($old) ?> users are 70+.<br>
+      <?php echo count($hs)?> users have graduated High School.<br>
+      <?php echo count($ad)?> users have Associates Degrees.<br>
+      <?php echo count($bd)?> users have Bachelors Degrees.<br>
+      <?php echo count($md)?> users have Masters Degrees.<br>
+      <?php echo count($phd)?> users have a PhD.</p>
         </main>
     <footer>
     <?php include $_SERVER['DOCUMENT_ROOT'].'/modules/footer.php'; ?>
--- assignments/survey/survey.php
+++ assignments/survey/survey.php
@@ -12,13 +12,10 @@
      </header>
      
       <p>
-          Take this quiz to see which Frozen character you are most like.
+          Please take this survey below.
       </p><br>
       <div id="survey_box">
-      <form action="process.php" method="post" id="survey_form">
-              <input type="hidden" name="action" value="continue">
-        
-          
+      <form action="reults.php" method="post" id="survey_form">
           <label>Enter your name:</label>
           <input type="text" name="name"><br>
           
@@ -26,7 +23,23 @@
           <input type="radio" name="gender" value="female">Female
           <input type="radio" name="gender" value="male">Male<br>
           
-          <input type="submit" value="Next">
+          <label>Select your age:</label>
+          <input type="radio" name="age" value="teen">0-19
+          <input type="radio" name="age" value="twenty">20-29
+          <input type="radio" name="age" value="thirty">30-39
+          <input type="radio" name="age" value="forty">40-49
+          <input type="radio" name="age" value="fifty">50-59
+          <input type="radio" name="age" value="sixty">60-69
+          <input type="radio" name="age" value="old">70+<br>
+          
+          <label>Education level (select all that apply):</label>
+          <input type="checkbox" name="education" value="hs">High School Graduate
+          <input type="checkbox" name="education" value="ad">Associate's Degree
+          <input type="checkbox" name="education" value="bd">Bachelor's Degree
+          <input type="checkbox" name="education" value="md">Master's Degree
+          <input type="checkbox" name="education" value="phd">PhD<br>
+          
+          <input type="submit" value="Submit">
           </form>
           
       </div>
