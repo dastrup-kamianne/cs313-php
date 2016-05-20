2042de6a50d4818a40545682cc7983026b72e77f
Dastrup's <Dastrup's>
2016-05-20 13:50:17 -0600

    structure

--- assignments/project/index.php
+++ assignments/project/index.php
@@ -6,11 +6,15 @@ $action = filter_input(INPUT_POST, 'action');
 if ($action == NULL) {
     $action = filter_input(INPUT_GET, 'action');
     if ($action == NULL) {
-        $action = 'get_patients';
+        $action = 'options';
     }
 }
 
 switch ($action){
+    case 'options':
+        include ('dentallist.php');
+        break;
+    
     case 'get_patients':
         include ('patient_list.php');
         break;
